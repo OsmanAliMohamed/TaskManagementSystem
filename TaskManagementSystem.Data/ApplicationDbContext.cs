3fe4f2cbf74ab941bdccd5fa94248b1384f9da12
osman <osmanali.software@gmail.com>
2024-09-02 18:58:38 +0300

    Add-Models&ProjectLayers

---
+++ TaskManagementSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Models.Models;
+using Attachment = TaskManagementSystem.Models.Models.Attachment;
+using Task = TaskManagementSystem.Models.Models.Task;
+
+
+namespace TaskManagementSystem.Data;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options)
+    {}
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Attachment> Attachments { get; set; }
+    public DbSet<AuditTrail> AuditTrails { get; set; }
+    public DbSet<TaskDependency> TaskDependencies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure many-to-many relationship between User and Team
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Teams)
+            .WithMany(t => t.Members)
+            .UsingEntity(j => j.ToTable("UserTeams"));
+
+        // Configure self-referencing many-to-many relationship for Task dependencies
+        modelBuilder.Entity<TaskDependency>()
+            .HasKey(td => new { td.TaskId, td.DependsOnTaskId });
+
+        modelBuilder.Entity<TaskDependency>()
+            .HasOne(td => td.Task)
+            .WithMany(t => t.Dependencies)
+            .HasForeignKey(td => td.TaskId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<TaskDependency>()
+            .HasOne(td => td.DependsOnTask)
+            .WithMany()
+            .HasForeignKey(td => td.DependsOnTaskId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
---
+++ TaskManagementSystem.Models/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Models.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public ICollection<User> Members { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
